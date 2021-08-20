7a84b39c56610bb92d3b53e1ab8621ce2ea4ff0a
ProgrammingFire <nouman.rahman2010@gmail.com>
2021-08-20 11:27:06 +0530

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+// MATERIAL UI
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
 
 function App() {
+  const [count, setCount] = useState<number | any>(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container style={{ textAlign: "center" }} maxWidth="md">
+      <Typography variant="h4">Your Counter Is : {count}</Typography>
+      <Button
+        style={{ margin: 7 }}
+        variant="outlined"
+        onClick={() => setCount(count + 1)}
+      >
+        Increment
+      </Button>
+      <Button
+        style={{ margin: 7 }}
+        variant="outlined"
+        onClick={() => setCount(count - 1)}
+      >
+        Decrement
+      </Button>
+    </Container>
   );
 }
 
